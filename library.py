f11c8f472a3c7a1ffbdf92a00bcc30473200c891
Talesa <talesahowell7@gmail.com>
2025-01-04 18:12:00 -0500

    added 4 files for class modules

---
+++ library.py
@@ -0,0 +1,5 @@
+class Library:
+    def __init__(self,books,users):
+        self.books = []
+        self.users = []
+        pass
---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, name,user_id,borrowed_book):
+        self.name = name
+        self.user_id = user_id
+        self.borrowed_book = borrowed_book
+        
+    
