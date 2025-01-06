ccee436ffd1cd5bdd141d50d3e193e225697648a
Talesa <Talesahowell7@gmail.com>
2025-01-06 18:05:16 -0500

    added to read me, and library methods

--- user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, name,user_id,borrowed_book):
-        self.name = name
-        self.user_id = user_id
-        self.borrowed_book = borrowed_book
-        
-    
